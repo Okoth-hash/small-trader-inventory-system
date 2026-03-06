df422b53976ae1676066c1c594e405ec3973cade
Robin Okoth <robinokoth023@gmail.com>
2026-03-06 15:47:15 +0300

    Fix credentials and BASE_URL for InfinityFree

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
-$host   = getenv('MYSQLHOST')     ?: 'sql213.infinityfree.com';
-$user   = getenv('MYSQLUSER')     ?: 'if0_41321571';
-$pass   = getenv('MYSQLPASSWORD') ?: 'Okothrobin1234';
-$dbname = getenv('MYSQLDATABASE') ?: 'if0_41321571_trader';
-$port   = (int)(getenv('MYSQLPORT') ?: 3306);
+$host   = 'sql213.infinityfree.com';
+$user   = 'if0_41321571';
+$pass   = 'Okothrobin1234';
+$dbname = 'if0_41321571_trader';
+$port   = 3306;
 
 $conn = new mysqli($host, $user, $pass, $dbname, $port);
 if ($conn->connect_error) {
