bfc7baaeca0eb0115b322a9027aaeda612bba829
Robin Okoth <robinokoth023@gmail.com>
2026-03-06 16:36:18 +0300

    Deploy to InfinityFree

--- config/database.php
+++ config/database.php
@@ -4,10 +4,7 @@ $user   = 'if0_41321571';
 $pass   = 'Okothrobin1234';
 $dbname = 'if0_41321571_trader';
 $port   = 3306;
-
 $conn = new mysqli($host, $user, $pass, $dbname, $port);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
 $conn->set_charset("utf8");
 ?>
